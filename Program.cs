db04c4bcfae62c439fedb5614314fb012b4684c7
Valentin447 <valentin@myttk.ru>
2022-11-08 19:36:47 +0700

    Add first loop

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿string[] arr = { "Test1", "aa", "hello", "(-:"};
+int quantityElements = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if(arr[i].Length <= 3)
+    {
+        quantityElements++;
+    }
+}
+string[] arrRes = new string[quantityElements];
